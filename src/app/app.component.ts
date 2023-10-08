ba523a8204d0e788e49feda10d78a0594ec264b2
Cristhian Rojas <103226786+CristhianJR4@users.noreply.github.com>
2023-10-08 17:08:52 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-test';
+  operandoA:string;
+  operandoB:string;
+  resultado:string;
+
+  onSumar():void {
+    this.resultado = this.operandoA.concat(this.operandoB);
+  }
 }
